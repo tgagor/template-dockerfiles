06441a9f2cccf4f21d323dc1e8f4765f677392d4
Tomasz Gągor <tomasz@gagor.pl>
2024-12-13 21:41:44 +0100

    feat!: drop Python code

--- templater/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Main application module."""
-
-from importlib.metadata import PackageNotFoundError, version
-
-try:
-    __version__ = version(__name__)
-except PackageNotFoundError:
-    __version__ = "unknown"
