59a9651849aedd162fdb67e949a03b61e8377cee
Tomasz Gągor <tomasz@gagor.pl>
2024-12-14 22:55:44 +0100

    fix: remove useless comments

--- pkg/runner/runner.go
+++ pkg/runner/runner.go
@@ -113,23 +113,3 @@ func (r Runner) RunParallel() error {
 
 	return <-results
 }
-
-// import (
-//     "sync"
-// )
-
-// func ExecuteTasks(cfg *Config, logger *Logger) {
-//     var wg sync.WaitGroup
-
-//     for _, task := range cfg.Images {
-//         wg.Add(1)
-//         go func(task ImageConfig) {
-//             defer wg.Done()
-//             logger.Info("Executing task for image: " + task.Dockerfile)
-//             // Do work here
-//         }(task)
-//     }
-
-//     wg.Wait()
-//     logger.Info("All tasks completed")
-// }
