8233a72a6457ebd5c3333e5dbb6532f6346c6cc1
Tomasz Gągor <tomasz@gagor.pl>
2025-01-06 22:30:45 +0100

    fix: change plase where I call build

--- pkg/parser/parser.go
+++ pkg/parser/parser.go
@@ -11,6 +11,25 @@ import (
 )
 
 func Run(cfg *config.Config, flags *config.Flags) error {
+	var buildEngine builder.Builder
+	// Choose the build engine based on the flag
+	switch flags.Engine {
+	case "buildx":
+		buildEngine = &builder.BuildxBuilder{}
+	// case "kaniko":
+	// 	buildEngine = &builder.KanikoBuilder{}
+	// case "podman":
+	// 	buildEngine = &builder.PodmanBuilder{}
+	default:
+		buildEngine = &builder.DockerBuilder{}
+	}
+
+	if err := buildEngine.Init(); err != nil {
+		log.Error().Err(err).Msg("Failed to initialize builder.")
+		return err
+	}
+	buildEngine.SetFlags(flags)
+
 	for _, name := range cfg.ImageOrder {
 		// Build only what's provided by --image flag (single image)
 		if flags.Image != "" && name != flags.Image {
@@ -20,25 +39,6 @@ func Run(cfg *config.Config, flags *config.Flags) error {
 		imageCfg := cfg.Images[name]
 		log.Debug().Str("image", name).Interface("config", imageCfg).Msg("Parsing")
 		log.Debug().Interface("excludes", imageCfg.Excludes).Msg("Excluded config sets")
-
-		var buildEngine builder.Builder
-		// Choose the build engine based on the flag
-		switch flags.Engine {
-		case "buildx":
-			buildEngine = &builder.BuildxBuilder{}
-		// case "kaniko":
-		// 	buildEngine = &builder.KanikoBuilder{}
-		// case "podman":
-		// 	buildEngine = &builder.PodmanBuilder{}
-		default:
-			buildEngine = &builder.DockerBuilder{}
-		}
-
-		if err := buildEngine.Init(); err != nil {
-			log.Error().Err(err).Msg("Failed to initialize builder.")
-			return err
-		}
-		buildEngine.SetFlags(flags)
 
 		combinations := GenerateVariableCombinations(imageCfg.Variables)
 		for _, rawConfigSet := range combinations {
@@ -58,20 +58,21 @@ func Run(cfg *config.Config, flags *config.Flags) error {
 			buildEngine.Queue(img)
 		}
 
-		// execute the build queue
-		if err := buildEngine.Run(); err != nil {
-			log.Error().Err(err).Msg("Building failed with error, check error above. Exiting.")
-			return err
-		}
-
-		// Shutdown the builder
-		if err := buildEngine.Terminate(); err != nil {
-			log.Error().Err(err).Msg("Failed to shutdown builder.")
-			return err
-		}
-
 		fmt.Println("")
 	}
+
+	// execute the build queue
+	if err := buildEngine.Run(); err != nil {
+		log.Error().Err(err).Msg("Building failed with error, check error above. Exiting.")
+		return err
+	}
+
+	// Shutdown the builder
+	if err := buildEngine.Terminate(); err != nil {
+		log.Error().Err(err).Msg("Failed to shutdown builder.")
+		return err
+	}
+
 	return nil
 }
 
